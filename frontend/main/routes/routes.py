513157216125d146a245916be6d851f7e276b7c7
bruno <brunoromero342@gmail.com>
2022-09-09 21:27:37 -0300

    tp7

--- frontend/main/routes/routes.py
+++ frontend/main/routes/routes.py
@@ -11,3 +11,18 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/mi-perfil')
+def mi_perfil():
+    return render_template('mi_perfil.html')
+
+@app.route('/lista-poemas')
+def lista_poemas():
+    return render_template('lista_poemas.html')
+
